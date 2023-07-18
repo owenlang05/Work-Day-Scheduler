b30cbbd8489ecbad3a041cff84c02d60b769bc97
Owen Lang <owen.lang146@gmail.com>
2023-07-18 11:29:03 -0500

    added day format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
+
+  $('#currentDay').text(getDay())
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,3 +25,21 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+function getDay() {
+  currentDay = dayjs().format("dddd, MMMM D");
+  
+  switch(currentDay[currentDay.length - 1]){
+    case 1:
+      currentDay += "st";
+    case 2:
+      currentDay += "nd";
+    case 3:
+      currentDay += "rd";
+    default:
+      currentDay += "th";
+  }
+
+  return currentDay;
+}
