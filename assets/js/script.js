41a521ce6d3ff2580d8bce65ba6c025aaf5c4d18
Owen Lang <owen.lang146@gmail.com>
2023-07-19 21:49:48 -0500

    removed comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,20 +42,8 @@ $(function () {
     rootEl.append(tempCalendarEl);
   }
   
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
 
   $('#root').on('click', '.saveBtn', save);
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
   //displays the current date in the header of the page.
   $('#currentDay').text(getDay())
 });
